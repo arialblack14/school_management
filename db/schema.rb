35c7a2efa03efe270b8fead086bb834396e59ca1
lukasx <llukasxx@gmail.com>
2015-12-02 04:19:29 -0800

    Add role table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151202121008) do
+ActiveRecord::Schema.define(version: 20151202121837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "roles", force: :cascade do |t|
+    t.string "role"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
