1e03dec230c40b897b0c7b6790f68d3cdda7ab5e
lukasx <llukasxx@gmail.com>
2015-12-02 04:11:28 -0800

    Add first and last name to users

--- spec/features/main_page_spec.rb
+++ spec/features/main_page_spec.rb
@@ -1,6 +1,6 @@
 feature "Main page", js: true do
   scenario "visiting main page" do
     visit root_path
-    expect(page).to have_content 'Log in'
+    expect(page).to have_content 'Welcome, please log in to continue.'
   end
 end
