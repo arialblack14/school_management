6d2006c6835657ceec6f9a60e494aa5d4b5417bd
lukasx <llukasxx@gmail.com>
2015-12-15 04:04:15 -0800

    add pointer to cursor

--- app/assets/javascripts/components/teacher/teacher_nav.es6.jsx
+++ app/assets/javascripts/components/teacher/teacher_nav.es6.jsx
@@ -11,11 +11,11 @@ class TeacherNav extends React.Component {
           </div>
           <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul className="nav navbar-nav">
-              <li><a id="groups" onClick={this.props.handleActiveTab}>Groups</a></li>
-              <li><a id="lessons" onClick={this.props.handleActiveTab}>Lessons</a></li>
-              <li><a id="students" onClick={this.props.handleActiveTab}>Students</a></li> 
-              <li><a id="events" onClick={this.props.handleActiveTab}>Events</a></li>
-              <li><a id="messages" onClick={this.props.handleActiveTab}>Messages</a></li>           
+              <li><a id="groups" className="nav-links" onClick={this.props.handleActiveTab}>Groups</a></li>
+              <li><a id="lessons" className="nav-links" onClick={this.props.handleActiveTab}>Lessons</a></li>
+              <li><a id="students" className="nav-links" onClick={this.props.handleActiveTab}>Students</a></li> 
+              <li><a id="events" className="nav-links" onClick={this.props.handleActiveTab}>Events</a></li>
+              <li><a id="messages" className="nav-links" onClick={this.props.handleActiveTab}>Messages</a></li>           
             </ul>
             <ul className="nav navbar-nav navbar-right">
               <li><a>Signed in as <b>{this.props.currentUser}</b></a></li>
