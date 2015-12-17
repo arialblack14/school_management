b4631ca139bb06a6cf003e2634d1b977e9a35bc2
lukasx <llukasxx@gmail.com>
2015-12-17 09:31:00 -0800

    Group list item display

--- app/assets/javascripts/components/teacher/groups/teacher_group_list.es6.jsx
+++ app/assets/javascripts/components/teacher/groups/teacher_group_list.es6.jsx
@@ -8,12 +8,15 @@ class TeacherGroupList extends React.Component {
         <div className="panel panel-info">
           <div className="panel-heading">Groups</div>
           <div className="panel-body">
-            <p>Hello {this.props.currentUser}, here you can see groups you're assinged in.</p>
+            <p>Hello {this.props.currentUser}, here you can see groups you're assinged in. 
+            Click on each if you want to see more info.</p>
           </div>
           <ul className="list-group">
             {this.props.groups.map(function(element){
-              return <TeacherGroupListItem group={element} key={element.id} />
-            })}
+              return <TeacherGroupListItem 
+                      group={element}
+                      key={element.id}/>
+            }, this)}
           </ul>
         </div>
       </div>
--- app/assets/javascripts/components/teacher/groups/teacher_group_list_item.es6.jsx
+++ app/assets/javascripts/components/teacher/groups/teacher_group_list_item.es6.jsx
@@ -1,10 +1,30 @@
 class TeacherGroupListItem extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {visible: false};
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    let visibility = !this.state.visible;
+    this.setState({visible: visibility});
   }
   render() {
     return (
-      <li className="list-group-item" key={this.props.group.id}>{this.props.group.name}</li>
+      <div onClick={this.handleClick}>
+        <li className="list-group-item highlight-hover" 
+            key={this.props.group.id}>
+            <b>{this.props.group.name}</b>
+          <div className={this.state.visible ? "" : "hidden" }>
+            <hr />
+            <p>
+              <b>Next lesson:</b>
+            </p>
+            <p>
+              <b>Number of students:</b>
+            </p>
+          </div>
+        </li>
+      </div>
     )
   }
 }
