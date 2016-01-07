48a043f46376fc4d96f8ce7b8f5fe056d0916390
lukasx <llukasxx@gmail.com>
2016-01-07 03:10:35 -0800

    Receivers boxes

--- app/assets/javascripts/components/teacher/messages/teacher_message_individual_list.es6.jsx
+++ app/assets/javascripts/components/teacher/messages/teacher_message_individual_list.es6.jsx
@@ -1,11 +1,35 @@
 class TeacherMessageIndividualList extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {users: ""};
+  }
+  componentDidMount() {
+    $.get("/get_users", function(data){
+      this.setState({users: data.teacher_dashboard});
+    }.bind(this));
   }
   render() {
+    let userListItems = [];
+    if(this.state.users.length > 0) {
+      this.state.users.map(function(element){
+        userListItems.push(<TeacherMessageIndividualListItem key={element.id} name={element.name}/>);
+      });
+    } else {
+      userListItems = <span className="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span>;
+    }
     return(
-      <div>
-        individual list
+      <div className="col-md-6">
+        <div className="panel panel-danger">
+          <div className="panel-heading">
+            <h3 className="panel-title"><b>User listing</b></h3>
+          </div>
+          <div className="panel-body">
+            <p>Pick person</p>
+            <ul className="nav nav-pills">
+              {userListItems}
+            </ul>
+          </div>
+        </div>
       </div>
     )
   }
---
+++ app/assets/javascripts/components/teacher/messages/teacher_message_individual_list_item.es6.jsx
@@ -0,0 +1,13 @@
+class TeacherMessageIndividualListItem extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return(
+      <li className="nav-links" 
+        role="presentation">
+        <a>{this.props.name} <span className="glyphicon glyphicon-plus"/></a>
+      </li>
+    )
+  }
+}
